928d947d00f9d32f3a6a44c0e462b3802654e580
devpark435 <devpark435@gmail.com>
2023-06-01 10:20:13 +0900

    modified : guidlistview resize mobile view

--- lib/screens/guid_list.dart
+++ lib/screens/guid_list.dart
@@ -36,6 +36,7 @@ class _QuestionListViewState extends State<QuestionListView> {
   String showAnswer = "";
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return FutureBuilder(
         future: GuidService().guidList(),
         builder: (context, snapshot) {
@@ -46,91 +47,191 @@ class _QuestionListViewState extends State<QuestionListView> {
           } else {
             if (snapshot.connectionState == ConnectionState.done) {
               return SizedBox(
-                  width: MediaQuery.of(context).size.width * .9,
+                  width: width * .9,
                   height: MediaQuery.of(context).size.width,
-                  child: Row(
-                    children: [
-                      Expanded(
-                          flex: 1,
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: ListView.builder(
-                                itemCount: snapshot.data!.questionList!.length,
-                                itemBuilder: (context, index) {
-                                  var question = snapshot
-                                      .data!.questionList![index].question;
-                                  var answer = snapshot
-                                      .data!.questionList![index].answer;
-                                  /**이부분 부터 작업해야함 */
-                                  return Card(
-                                      child: Column(
-                                    children: [
-                                      ListTile(
-                                        title: Row(
+                  child: width > 700
+                      ? Row(
+                          children: [
+                            Expanded(
+                                flex: 1,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: ListView.builder(
+                                      itemCount:
+                                          snapshot.data!.questionList!.length,
+                                      itemBuilder: (context, index) {
+                                        var question = snapshot.data!
+                                            .questionList![index].question;
+                                        var answer = snapshot
+                                            .data!.questionList![index].answer;
+                                        /**이부분 부터 작업해야함 */
+                                        return Card(
+                                            child: Column(
                                           children: [
-                                            Typografie().labelMedium(
-                                                "$question", Colors.black),
-                                            const Icon(
-                                              Icons.question_answer_outlined,
-                                              size: 24,
+                                            ListTile(
+                                              title: Row(
+                                                children: [
+                                                  Typografie().labelMedium(
+                                                      "$question",
+                                                      Colors.black),
+                                                  const Icon(
+                                                    Icons
+                                                        .question_answer_outlined,
+                                                    size: 24,
+                                                  ),
+                                                ],
+                                              ),
+                                              onTap: () {
+                                                setState(() {
+                                                  showQuestion = "$question";
+                                                  showAnswer = "$answer";
+                                                });
+                                              },
                                             ),
                                           ],
-                                        ),
-                                        onTap: () {
-                                          setState(() {
-                                            showQuestion = "$question";
-                                            showAnswer = "$answer";
-                                          });
-                                        },
-                                      ),
-                                    ],
-                                  ));
-                                }),
-                          )),
-                      Expanded(
-                          flex: 1,
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Container(
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(25),
-                                  border: Border.all(
-                                      width: 1, color: Colors.black38)),
-                              child: Column(
-                                children: [
-                                  Expanded(
-                                      flex: 1,
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(
-                                            left: 20.0, top: 10),
-                                        child: Container(
-                                            alignment: Alignment.centerLeft,
-                                            child: Row(
-                                              children: [
-                                                Typografie().labelLarge(
-                                                    showQuestion, Colors.black),
-                                                Icon(Icons
-                                                    .question_answer_rounded)
-                                              ],
+                                        ));
+                                      }),
+                                )),
+                            Expanded(
+                                flex: 1,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(25),
+                                        border: Border.all(
+                                            width: 1, color: Colors.black38)),
+                                    child: Column(
+                                      children: [
+                                        Expanded(
+                                            flex: 1,
+                                            child: Padding(
+                                              padding: const EdgeInsets.only(
+                                                  left: 20.0, top: 10),
+                                              child: Container(
+                                                  alignment:
+                                                      Alignment.centerLeft,
+                                                  child: Row(
+                                                    children: [
+                                                      Typografie().labelLarge(
+                                                          showQuestion,
+                                                          Colors.black),
+                                                      Icon(Icons
+                                                          .question_answer_rounded)
+                                                    ],
+                                                  )),
                                             )),
-                                      )),
-                                  Expanded(
-                                      flex: 5,
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(
-                                            left: 20.0, top: 10, right: 20),
-                                        child: Container(
-                                            alignment: Alignment.topLeft,
-                                            child: Typografie().labelMedium(
-                                                showAnswer, Colors.black)),
-                                      )),
-                                ],
-                              ),
-                            ),
-                          ))
-                    ],
-                  ));
+                                        Expanded(
+                                            flex: 5,
+                                            child: Padding(
+                                              padding: const EdgeInsets.only(
+                                                  left: 20.0,
+                                                  top: 10,
+                                                  right: 20),
+                                              child: Container(
+                                                  alignment: Alignment.topLeft,
+                                                  child: Typografie()
+                                                      .labelMedium(showAnswer,
+                                                          Colors.black)),
+                                            )),
+                                      ],
+                                    ),
+                                  ),
+                                ))
+                          ],
+                        )
+                      : Column(
+                          children: [
+                            Expanded(
+                                flex: 2,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: ListView.builder(
+                                      itemCount:
+                                          snapshot.data!.questionList!.length,
+                                      itemBuilder: (context, index) {
+                                        var question = snapshot.data!
+                                            .questionList![index].question;
+                                        var answer = snapshot
+                                            .data!.questionList![index].answer;
+                                        /**이부분 부터 작업해야함 */
+                                        return Card(
+                                            child: Column(
+                                          children: [
+                                            ListTile(
+                                              title: Row(
+                                                children: [
+                                                  Typografie().labelSamll(
+                                                      "$question",
+                                                      Colors.black),
+                                                  const Icon(
+                                                    Icons
+                                                        .question_answer_outlined,
+                                                    size: 24,
+                                                  ),
+                                                ],
+                                              ),
+                                              onTap: () {
+                                                setState(() {
+                                                  showQuestion = "$question";
+                                                  showAnswer = "$answer";
+                                                });
+                                              },
+                                            ),
+                                          ],
+                                        ));
+                                      }),
+                                )),
+                            Expanded(
+                                flex: 3,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(25),
+                                        border: Border.all(
+                                            width: 1, color: Colors.black38)),
+                                    child: Column(
+                                      children: [
+                                        Expanded(
+                                            flex: 1,
+                                            child: Padding(
+                                              padding: const EdgeInsets.only(
+                                                  left: 20.0, top: 10),
+                                              child: Container(
+                                                  alignment:
+                                                      Alignment.centerLeft,
+                                                  child: Row(
+                                                    children: [
+                                                      Typografie().labelSamll(
+                                                          showQuestion,
+                                                          Colors.black),
+                                                      Icon(Icons
+                                                          .question_answer_rounded)
+                                                    ],
+                                                  )),
+                                            )),
+                                        Expanded(
+                                            flex: 5,
+                                            child: Padding(
+                                              padding: const EdgeInsets.only(
+                                                  left: 20.0,
+                                                  top: 10,
+                                                  right: 20),
+                                              child: Container(
+                                                  alignment: Alignment.topLeft,
+                                                  child: Typografie()
+                                                      .labelSamll(showAnswer,
+                                                          Colors.black)),
+                                            )),
+                                      ],
+                                    ),
+                                  ),
+                                ))
+                          ],
+                        ));
             } else if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(
                 child: CircularProgressIndicator(),
